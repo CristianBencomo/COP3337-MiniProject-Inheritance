8f3bafbb24838487508210f08de0065344f04fb9
CristianBencomo <cristian.bencomo@outlook.com>
2020-06-02 16:39:25 -0400

    Windows machine: Started on Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,18 @@
 //package app;
 
+
+import Car; //import machine.Car;
+import Motorcycle; //import machine.Motorcycle;
+import SUV; //import machine.SUV;
+import Vehicle; //import machine.Vehicle;
+
+
 public class Controller {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
+        Vehicle vehicle = new Vehicle;
+        
     }
     
 }
